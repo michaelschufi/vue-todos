d6f78b778fcdfe69562fd2f324fef90453545129
Michael Schaufelberger <michael.schufi@gmail.com>
2018-06-07 13:54:32 +0200

    api-server: added socket.io as dependency and updated syntax

--- api-server/production.js
+++ api-server/production.js
@@ -1,37 +1,38 @@
 // hello-server-attach.js
-var PORT = process.env.PORT || 2403;
-var ENV = process.env.NODE_ENV || "development";
+const PORT = process.env.PORT || 2403
+const ENV = process.env.NODE_ENV || 'development'
 
 // setup http + express + socket.io
-var fs = require("fs");
-var express = require("express");
-var app = express();
+const fs = require('fs')
+const express = require('express')
 
-var privateKey = fs.readFileSync("./privkey.pem", "utf8");
-var certificate = fs.readFileSync("./cert.pem", "utf8");
+const app = express()
 
-var credentials = { key: privateKey, cert: certificate };
+const privateKey = fs.readFileSync('./privkey.pem', 'utf8')
+const certificate = fs.readFileSync('./cert.pem', 'utf8')
 
-var server = require("https").createServer(credentials, app);
-var io = require("socket.io").listen(server, { "log level": 0 });
+const credentials = { key: privateKey, cert: certificate }
+
+const server = require('https').createServer(credentials, app)
+const io = require('socket.io').listen(server, { 'log level': 0 })
 
 // setup deployd
-require("deployd").attach(server, {
+require('deployd').attach(server, {
   socketIo: io, // if not provided, attach will create one for you.
   env: ENV,
   db: {
-    host: "localhost",
+    host: 'localhost',
     port: 27017,
-    name: "todos",
+    name: 'todos',
     credentials: {
-      username: "deployd",
-      password: ""
-    }
-  }
-});
+      username: 'deployd',
+      password: '',
+    },
+  },
+})
 
 // After attach, express can use server.handleRequest as middleware
-app.use(server.handleRequest);
+app.use(server.handleRequest)
 
 // start server
-server.listen(PORT);
+server.listen(PORT)
